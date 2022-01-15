cd5b140c8679b1e71ee84d13b9e0b05f54698284
Maria Eugenia Costa <costamariaeugenia1@gmail.com>
2022-01-15 20:04:02 -0300

    modifico el js del color flipper y counter

--- js/color_flipper_counter.js
+++ js/color_flipper_counter.js
@@ -1,15 +1,14 @@
 // A colors variable, an array with the values
-const colors =["green", "red", "rgba(133, 122, 200)", "#f1f5f8"];
+const colors = ["green", "red", "rgba(133, 122, 200)", "#f1f5f8"];
 // target the button
-const btn = document.getElementById("btn");
+const btn = document.getElementById("btn1");
 //target the span with the class color
 const color = document.querySelector(".color");
 
 // the event listener that will call the function once we click
-btn.addEventListener("click", function() {
+btn.addEventListener("click", function () {
   //get a random number between 0 and 3, the length of the color array
   const randomNumber = getRandomNumber();
-  console.log(randomNumber);
 
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
