4c636138a9f8c58229b5df192a295506c96cd06e
Maria Eugenia Costa <costamariaeugenia1@gmail.com>
2022-01-15 19:21:40 -0300

    agrego los estilos al ajercicio que cambia el color de fondo cuando se hace click en el boton, falta ajustar el codigo JS para que se ejecute el AddEventlistener

---
+++ js/color_flipper.js
@@ -0,0 +1,21 @@
+// A colors variable, an array with the values
+const colors =["green", "red", "rgba(133, 122, 200)", "#f1f5f8"];
+// target the button
+const btn = document.getElementById("btn");
+//target the span with the class color
+const color = document.querySelector(".color");
+
+// the event listener that will call the function once we click
+btn.addEventListener("click", function() {
+  //get a random number between 0 and 3, the length of the color array
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+// function to generate the random number between 0 and 3
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
